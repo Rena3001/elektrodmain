0c256759d9416fbc2e47b2233128f242b7c5eff3
gabilgg07 <gabilgg07@gmail.com>
2024-06-16 14:33:29 +0400

    All resource Controller added in routes

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,23 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\admin\AdminController;
 use App\Http\Controllers\Admin\AuthController;
+use App\Http\Controllers\Admin\BlogController;
+use App\Http\Controllers\Admin\BranchController;
+use App\Http\Controllers\Admin\BrandController;
+use App\Http\Controllers\Admin\CastingController;
+use App\Http\Controllers\Admin\ContactCardController;
+use App\Http\Controllers\Admin\DirectorController;
 use App\Http\Controllers\Admin\LangController;
 use App\Http\Controllers\Admin\LanguageLineController;
+use App\Http\Controllers\Admin\PageController;
+use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Admin\SliderController;
+use App\Http\Controllers\Admin\TimeLineController;
 use App\Http\Controllers\Admin\ValveCategoryController;
+use App\Http\Controllers\Admin\ValveSliderController;
+use App\Http\Controllers\Admin\WeldingCategoryController;
+use App\Http\Controllers\Admin\WeldingGroupController;
+use App\Http\Controllers\Admin\WeldingSliderController;
 use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
 // Front
@@ -24,6 +37,19 @@ Route::group(['middleware' => 'auth', 'prefix' => LaravelLocalization::setLocale
     Route::resource('langs', LangController::class);
     Route::resource('valve_categories', ValveCategoryController::class);
     Route::resource('sliders', SliderController::class);
+    Route::resource('blogs', BlogController::class);
+    Route::resource('branchs', BranchController::class);
+    Route::resource('brands', BrandController::class);
+    Route::resource('castings', CastingController::class);
+    Route::resource('contact_cards', ContactCardController::class);
+    Route::resource('directors', DirectorController::class);
+    Route::resource('pages', PageController::class);
+    Route::resource('products', ProductController::class);
+    Route::resource('time_lines', TimeLineController::class);
+    Route::resource('valve_sliders', ValveSliderController::class);
+    Route::resource('welding_categories', WeldingCategoryController::class);
+    Route::resource('welding_groups', WeldingGroupController::class);
+    Route::resource('welding_sliders', WeldingSliderController::class);
 });
 
 // Admin Login
